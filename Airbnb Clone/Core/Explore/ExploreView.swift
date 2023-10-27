da10286699464f514f7bd0a634c026e8ddad6c8f
A-arnaavv <saketarnav2005@gmail.com>
2023-10-27 22:06:41 +0530

    Custom Serach Bar

--- Airbnb Clone/Core/Explore/ExploreView.swift
+++ Airbnb Clone/Core/Explore/ExploreView.swift
@@ -10,19 +10,21 @@ import SwiftUI
 struct ExploreView: View {
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVStack(spacing: 32) {
-                    ForEach(0 ... 10, id: \.self) { listing in
-                        ListingItemView()
-                            .frame(height: 400)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                ScrollView {
+                    SearchAndFilterBar()
+                    
+                    LazyVStack(spacing: 32) {
+                        ForEach(0 ... 10, id: \.self) { listing in
+                            ListingItemView()
+                                .frame(height: 400)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
                     }
+                    .padding()
                 }
-                .padding()
             }
         }
     }
-}
 
 #Preview {
     ExploreView()
