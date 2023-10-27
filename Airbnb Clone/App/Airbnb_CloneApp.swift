028eb9564bb8426d85d77271cb3869764d4cc612
A-arnaavv <saketarnav2005@gmail.com>
2023-10-27 21:20:33 +0530

    Explore View UI

---
+++ Airbnb Clone/App/Airbnb_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_CloneApp.swift
+//  Airbnb Clone
+//
+//  Created by Arnav Aggarwal on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
