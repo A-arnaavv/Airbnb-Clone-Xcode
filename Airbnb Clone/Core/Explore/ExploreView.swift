460cd2b45ac8aad67939bfa6ea850a1fbb4d756a
A-arnaavv <saketarnav2005@gmail.com>
2023-10-29 18:44:16 +0530

    Updated Listing Detail View

--- Airbnb Clone/Core/Explore/ExploreView.swift
+++ Airbnb Clone/Core/Explore/ExploreView.swift
@@ -25,7 +25,8 @@ struct ExploreView: View {
                     .padding()
                 }
                 .navigationDestination(for: Int.self) {listing in
-                        Text("Lisitng Detail View...")
+                    ListingDetailView()
+                        .navigationBarBackButtonHidden()
                 }
             }
         }
